a814a8ee13307daec1de3f5e38775dfbd401c687
Utsha5 <68023409+Utsha-saha@users.noreply.github.com>
2023-07-20 01:44:00 -0700

    Add files via upload

---
+++ Group Anagrams.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res=collections.defaultdict(list)
+
+        for s in strs:
+            count = [0]*26
+            for c in s:
+                
+                count[ord(c) - ord("a")] += 1
+            res[tuple(count)].append(s)
+        return res.values()
+
